46af6cdd8183a719d118bd0c671501dafd55d9ac
xDaniel-R <ce.caceres@duocuc.cl>
2025-04-15 14:27:22 -0400

    Subiendo microservicio de Pagos y Notificaciones

---
+++ pagos-notificaciones/src/main/java/com/pagosnotificaciones/pagos_notificaciones_api/PagosNotificacionesApplication.java
@@ -0,0 +1,11 @@
+package com.pagosnotificaciones.pagos_notificaciones_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosNotificacionesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PagosNotificacionesApplication.class, args);
+    }
+}
---
+++ pagos-notificaciones/src/test/java/com/pagosnotificaciones/pagos_notificaciones_api/PagosNotificacionesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagosnotificaciones.pagos_notificaciones_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosNotificacionesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
