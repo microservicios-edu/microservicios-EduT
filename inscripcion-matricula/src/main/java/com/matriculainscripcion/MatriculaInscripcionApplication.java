5cd6fcc329075b2632899722ab635d559cc21402
xDani-R <rodriguez.cesar725@gmail.com>
2025-04-17 15:06:12 -0400

    Subiendo microservicio de Matricula y Inscripcion

---
+++ inscripcion-matricula/src/main/java/com/matriculainscripcion/MatriculaInscripcionApplication.java
@@ -0,0 +1,13 @@
+package com.matriculainscripcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculaInscripcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculaInscripcionApplication.class, args);
+	}
+
+}
---
+++ inscripcion-matricula/src/test/java/com/matriculainscripcion/MatriculaInscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matriculainscripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculaInscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
