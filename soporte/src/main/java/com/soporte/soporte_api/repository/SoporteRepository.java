9dc9d3e37b7e7eb6b3ec8a3e6c2e61fe30b59b0b
INMERCION <om.felipe@duocuc.cl>
2025-05-05 20:27:24 -0400

    Subiendo y actualizando Soporte

--- soporte/src/main/java/com/soporte/soporte_api/repository/SoporteRepository.java
+++ soporte/src/main/java/com/soporte/soporte_api/repository/SoporteRepository.java
@@ -1,3 +1,11 @@
-public class SoporteRepository {
-//pendiente de codificar
+package com.soporte.soporte_api.repository;
+
+import com.soporte.soporte_api.model.Soporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoporteRepository extends JpaRepository<Soporte, Integer> {
+    // Puedes agregar métodos personalizados si necesitas, pero no es obligatorio
 }
+
