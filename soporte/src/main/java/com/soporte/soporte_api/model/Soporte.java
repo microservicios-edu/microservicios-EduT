ec4d5c520a69794b86fcf8541502a242a21c2a1d
Diana3180chav <dia.chavez@duocuc.cl>
2025-05-12 10:24:21 -0400

    Agrego validaciones y modifico microservicio Soporte.java

--- soporte/src/main/java/com/soporte/soporte_api/model/Soporte.java
+++ soporte/src/main/java/com/soporte/soporte_api/model/Soporte.java
@@ -1,3 +1,24 @@
+package com.soporte.soporte_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "soporte")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Soporte {
-//pendiente de codificar
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombreUsuario;
+    private String correo;
+    private String comentario;
+    private String estado;
+    private String fechaCreacion;
 }
